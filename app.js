250d55c8d990d38f841ef91e6bb5abe3f1beafd4
Mohamed Ouf <ouf@aydi.com>
2024-10-20 12:19:09 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   app.js

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World", date: new Date().toISOString() });
+  res.json({
+    message: "Hello World",
+    date: new Date().toISOString(),
+    process: process.env,
+    project: "worker",
+  });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(4000, () => {
+  console.log("Server is running on port  4000");
 });
