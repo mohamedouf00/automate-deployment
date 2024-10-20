9b50f2807eacd26bbe83f77ede0c1c88fffa95e2
Mohamed Ouf <ouf@aydi.com>
2024-10-20 12:13:13 +0300

    Add initial server setup and dependencies

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World", date: new Date().toISOString() });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
